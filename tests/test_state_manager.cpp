1ee8197e608c3f40afab552722c5f21717a21268
1nfern006 <alisa.maskina716@gmail.com>
2026-03-12 20:11:01 +0300

    feat: add tests to state manager

--- tests/test_state_manager.cpp
+++ tests/test_state_manager.cpp
@@ -26,3 +26,17 @@ TEST(StateManagerTest, PopDoesNotCrashOnEmpty) {
     StateManager sm;
     EXPECT_NO_THROW(sm.Pop());
 }
+
+
+
+TEST(StateManagerTest, ChangeStateReplacesCurrent) {
+    StateManager sm;
+    sm.Push(std::make_unique<FakeState>());
+    
+    auto newState = std::make_unique<FakeState>();
+    FakeState* newPtr = newState.get(); 
+    
+    sm.Change(std::move(newState));
+    sm.Update(1.0f);
+    EXPECT_TRUE(newPtr->isUpdated);
+}
